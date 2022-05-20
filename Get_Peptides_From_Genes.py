66e9b712a087fa040c81db2bc3e5a267cbd15cad
mo11 <mo11@sanger.ac.uk>
2022-05-20 12:10:26 +0100

    changed code for v3.9

--- Get_Peptides_From_Genes.py
+++ Get_Peptides_From_Genes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #Peptide = "LLVLH";
 #count_number_of_ocurances_in_MSP(Peptide)
 #Main
@@ -81,7 +83,7 @@ def process_peptides_matrix(input):
                     peptide_length = len(peptide)
 
                     #the cut of thereshold.
-                    if peptide_length>3 and peptide_length<8:
+                    if peptide_length>2 and peptide_length<8:
                         peptide_info=peptide_info.append({'GN': The_protein_gene_name_to_search_for, 'Enzyme': str(Enzyme1)+' and '+ str(Enzyme2), 'start_cleavage_site': start_cleavage_site, 'end_cleavage_site': end_cleavage_site, 'average_cleavage_score': average_cleavage_score, 'length_of_peptide': peptide_length, 'peptide': peptide}, ignore_index=True)
                     else:
                         pass
@@ -90,11 +92,10 @@ def process_peptides_matrix(input):
 
 
         except TypeError:
-            print 'No Peptides'
+            print('No Peptides')
             peptide_info=pd.DataFrame()
     count=count+1
     return peptide_info
-
 def get_protein_sequence(Gene):
 
     Position_of_Gene = grep(Protein_info.iloc[:, 0], Gene)
@@ -104,7 +105,7 @@ def get_protein_sequence(Gene):
         # print 'no gene found'
         import Tkinter
         import tkMessageBox
-        print "Gene Not Found in Your Dataset"
+        print("Gene Not Found in Your Dataset")
         tkMessageBox.showwarning('Error', 'Gene Name Not Found')
         return 'pass'
 
@@ -191,101 +192,99 @@ def count_number_of_ocurances_in_MSP(peptide):
         count = count + 1
     peptide_proteins_domains = ';'.join(a_list_with_ids_of_peptide_cotaining_proteins)
     #print peptide_proteins_domains;
-    print count_occurances;
-
+    print(count_occurances);
 
 #files we are working with
 
+if __name__ == '__main__':
+    Protein_domains2 = pd.read_csv('bin/Domain_Info.csv', index_col=False)
+    Protein_Protease_sites = pd.read_csv('bin/Prosper.csv', index_col=False)
+    Protein_info = pd.read_csv('bin/Summary_MSP_E.csv', index_col=False)
 
-Protein_domains2 = pd.read_csv('bin/Domain_Info.csv', index_col=False)
-Protein_Protease_sites = pd.read_csv('bin/Prosper.csv', index_col=False)
-Protein_info = pd.read_csv('bin/Summary_MSP_E.csv', index_col=False, tupleize_cols=False)
+    protein_gene_name=["HIST1H3A","HGFAC","HGF","HADHA","H3F3A","GSN","GRP","GPX3","GPI","GPC2","GPC1","GNLY","GLIPR2","GLG1","GLA","GHR","GGH","GDNF","GBP1","GBA","GAPDH","GANAB","FST","FRAS1","FOLR2","FN1","FMOD","FLT1","FLNB","FLNA","FKBP1A","FGG","FGFR4","FGFR3","FGFR2","FGFR1","FGF7","FGF3","FGF2","FGB","FGA","FBN2","FBN1","FBLN5","FBLN2","FBLN1","FAM3C","F2R","F2","ESR2","ERAP1","EPO","EPHB4","ENDOD1","EMILIN1","EMCN","ELN","EGFR","EFTUD2","EFNA3","EFNA1","EFEMP2","EFEMP1","EEF2","EDN1","ECM1","DYNC1H1","DSG1","DSC2","DPT","DPP7","DMKN","DMBT1","DEFB4A","DEFB1","DEFA3","DEFA1","DCN","DCD","DAG1","CXCL9","CXCL8","CXCL12","CXCL1","CXADR","CTSV","CTSK","CTSG","CTSD","CTHRC1","CTGF","CSTA","CST6","CST4","CST3","CSPG4","CRTAP","CRIP2","CRH","CREG1","CPQ","CPE","CPA3","CP","COPA","COMP","COLQ","COL7A1","COL6A6","COL6A5","COL6A3","COL6A2","COL6A1","COL5A2","COL5A1","COL4A6","COL4A5","COL4A4","COL4A3","COL4A2","COL4A1","COL3A1","COL2A1","COL1A2","COL1A1","COL18A1","COL17A1","COL16A1","COL15A1","COL14A1","COL12A1","CMA1","CLU","CLTC","CLCA4","CKLF","CKAP4","CHGB","CHGA","CFI","CFHR3","CFHR1","CFH","CFD","CFB","CELA1","CDSN","CDH1","CD8B","CD8A","CD5L","CD55","CD40","CD34","CD209","CD163","CD14","CD109","CCT6A","CCT2","CCL7","CCL5","CCL27","CCL26","CCL2","CCL11","CASP4","CASP1","CAPZA2","CAPZA1","CANX","CAMP","CALU","CALR","CALM1","C9","C8B","C7","C5","C4BPA","C4B","C4A","C3","C1S","C1R","C1QC","C1QBP","C1QB","BGN","BDNF","B2M","AZGP1","ATP5O","ATP5B","ATP5A1","ASPN","ARF4","APOH","APOE","APOD","APOB","APOA4","APOA2","APOA1","APCS","ANXA2P2","ANXA2","ANXA1","ANOS1","ANGPT2","ANGPT1","AMBP","ALDOA","ALCAM","ALB","AIMP1","AHSG","AGTR2","AGT","AFM","AEBP1","ADM2","ADM","ADIPOQ","ADAMTSL5","ADAMTS17","ACTN4","ACTN2","ACTN1","ACPP","ACE2","ACE","ABI3BP","A2ML1","A2M","A1BG"]
+    all_the_protease_ids=['C01.036','M10.003','M10.004', 'M10.005','M10.008','S01.131','S01.133','S01.010'];
 
+    for The_protein_gene_name_to_search_for in protein_gene_name :
+        print(f"Analysing: {The_protein_gene_name_to_search_for}")
 
-protein_gene_name=["HIST1H3A","HGFAC","HGF","HADHA","H3F3A","GSN","GRP","GPX3","GPI","GPC2","GPC1","GNLY","GLIPR2","GLG1","GLA","GHR","GGH","GDNF","GBP1","GBA","GAPDH","GANAB","FST","FRAS1","FOLR2","FN1","FMOD","FLT1","FLNB","FLNA","FKBP1A","FGG","FGFR4","FGFR3","FGFR2","FGFR1","FGF7","FGF3","FGF2","FGB","FGA","FBN2","FBN1","FBLN5","FBLN2","FBLN1","FAM3C","F2R","F2","ESR2","ERAP1","EPO","EPHB4","ENDOD1","EMILIN1","EMCN","ELN","EGFR","EFTUD2","EFNA3","EFNA1","EFEMP2","EFEMP1","EEF2","EDN1","ECM1","DYNC1H1","DSG1","DSC2","DPT","DPP7","DMKN","DMBT1","DEFB4A","DEFB1","DEFA3","DEFA1","DCN","DCD","DAG1","CXCL9","CXCL8","CXCL12","CXCL1","CXADR","CTSV","CTSK","CTSG","CTSD","CTHRC1","CTGF","CSTA","CST6","CST4","CST3","CSPG4","CRTAP","CRIP2","CRH","CREG1","CPQ","CPE","CPA3","CP","COPA","COMP","COLQ","COL7A1","COL6A6","COL6A5","COL6A3","COL6A2","COL6A1","COL5A2","COL5A1","COL4A6","COL4A5","COL4A4","COL4A3","COL4A2","COL4A1","COL3A1","COL2A1","COL1A2","COL1A1","COL18A1","COL17A1","COL16A1","COL15A1","COL14A1","COL12A1","CMA1","CLU","CLTC","CLCA4","CKLF","CKAP4","CHGB","CHGA","CFI","CFHR3","CFHR1","CFH","CFD","CFB","CELA1","CDSN","CDH1","CD8B","CD8A","CD5L","CD55","CD40","CD34","CD209","CD163","CD14","CD109","CCT6A","CCT2","CCL7","CCL5","CCL27","CCL26","CCL2","CCL11","CASP4","CASP1","CAPZA2","CAPZA1","CANX","CAMP","CALU","CALR","CALM1","C9","C8B","C7","C5","C4BPA","C4B","C4A","C3","C1S","C1R","C1QC","C1QBP","C1QB","BGN","BDNF","B2M","AZGP1","ATP5O","ATP5B","ATP5A1","ASPN","ARF4","APOH","APOE","APOD","APOB","APOA4","APOA2","APOA1","APCS","ANXA2P2","ANXA2","ANXA1","ANOS1","ANGPT2","ANGPT1","AMBP","ALDOA","ALCAM","ALB","AIMP1","AHSG","AGTR2","AGT","AFM","AEBP1","ADM2","ADM","ADIPOQ","ADAMTSL5","ADAMTS17","ACTN4","ACTN2","ACTN1","ACPP","ACE2","ACE","ABI3BP","A2ML1","A2M","A1BG"]
-all_the_protease_ids=['C01.036','M10.003','M10.004', 'M10.005','M10.008','S01.131','S01.133','S01.010'];
+        Position_of_Gene = grep(Protein_Protease_sites.iloc[:, 0], The_protein_gene_name_to_search_for)
+        cleavage_sites_for_gene = Protein_Protease_sites.iloc[Position_of_Gene, :]
 
-for The_protein_gene_name_to_search_for in protein_gene_name :
-    print "Analysing: "+The_protein_gene_name_to_search_for
-
-    Position_of_Gene = grep(Protein_Protease_sites.iloc[:, 0], The_protein_gene_name_to_search_for)
-    cleavage_sites_for_gene = Protein_Protease_sites.iloc[Position_of_Gene, :]
-
-    cleavage_sites_for_gene = get_protein_protease_cleavage_sites(The_protein_gene_name_to_search_for)
-
-
-    all_indexes=[];
-    for g in all_the_protease_ids:
-        indexes=grep(cleavage_sites_for_gene['ID'],g)
-        all_indexes=all_indexes+indexes
+        cleavage_sites_for_gene = get_protein_protease_cleavage_sites(The_protein_gene_name_to_search_for)
 
 
-    Protease_sites = cleavage_sites_for_gene.iloc[all_indexes, :]; #cleavage_sites_for_gene = all the cleavage sites for a specific gene name. all_indexes = all the columns that has the protease ID
-    Peptides=process_peptides_matrix(Protease_sites)
-    new_column_same_sequences_bio_f = pd.DataFrame()
+        all_indexes=[];
+        for g in all_the_protease_ids:
+            indexes=grep(cleavage_sites_for_gene['ID'],g)
+            all_indexes=all_indexes+indexes
 
 
-    #this part finds all the other sequences that contain this pattern
-
-    if (len(Peptides) < 1):
-        #print "true, it is smaler then 1"
-        pass
-    else:
+        Protease_sites = cleavage_sites_for_gene.iloc[all_indexes, :]; #cleavage_sites_for_gene = all the cleavage sites for a specific gene name. all_indexes = all the columns that has the protease ID
+        Peptides=process_peptides_matrix(Protease_sites)
+        new_column_same_sequences_bio_f = pd.DataFrame()
 
 
-        for peptide in Peptides['peptide']:
-            count = 0
-            a_list_with_ids_of_peptide_cotaining_proteins = []
+        #this part finds all the other sequences that contain this pattern
 
-            #loops through all the original sequences
-            for line in Protein_info[' The_original_sequence']:
-                try:
-                    if peptide in line:
-
-                        Gene = str(Protein_info['Protein Gene Name'][count])
-                        #print("peptide matched "+Gene)
-                        # check if the location is part of any domain
-                        Sequence = get_protein_sequence(Gene)
-                        Protein_domains = Get_protein_domains(Gene) #this finds the domains of the protein that the peptide has been matched with
-                        Start = find_str(Sequence, peptide)
-                        End = Start + len(peptide)
-
-                        for number2 in range(0, len(Protein_domains)):
-                            # susceptibility_of_domain = 0
-                            Domain_name = Protein_domains.iloc[number2, 3]
-
-                            Domain_start = Protein_domains.iloc[number2, 4]
-                            Domain_finish = Protein_domains.iloc[number2, 5]
-                            z = Start
-                            q = End
-                            x = Domain_start
-                            y = Domain_finish
-                            gene_peptide_domain = []
-                            if x <= z and z < y <= q or z <= x < q and y >= q or z <= x <= q and z <= y <= q or x <= z and y >= q and not x > q and y > q or not y < z and x < z:
-                                # if this domain is fully within.
-                                gene_peptide_domain = str(Gene + '(' + Domain_name + ')')
-
-                        if gene_peptide_domain == []:
-                            a_list_with_ids_of_peptide_cotaining_proteins = a_list_with_ids_of_peptide_cotaining_proteins + [
-                                str(Gene + '(' + ' ' + ')')]
-                        else:
-                            a_list_with_ids_of_peptide_cotaining_proteins = a_list_with_ids_of_peptide_cotaining_proteins + [
-                                str(gene_peptide_domain)]
+        if (len(Peptides) < 1):
+            #print "true, it is smaler then 1"
+            pass
+        else:
 
 
+            for peptide in Peptides['peptide']:
+                count = 0
+                a_list_with_ids_of_peptide_cotaining_proteins = []
 
-                except:
-                    pass
-                # print count
-                count = count + 1
+                #loops through all the original sequences
+                for line in Protein_info[' The_original_sequence']:
+                    try:
+                        if peptide in line:
+
+                            Gene = str(Protein_info['Protein Gene Name'][count])
+                            #print("peptide matched "+Gene)
+                            # check if the location is part of any domain
+                            Sequence = get_protein_sequence(Gene)
+                            Protein_domains = Get_protein_domains(Gene) #this finds the domains of the protein that the peptide has been matched with
+                            Start = find_str(Sequence, peptide)
+                            End = Start + len(peptide)
+
+                            for number2 in range(0, len(Protein_domains)):
+                                # susceptibility_of_domain = 0
+                                Domain_name = Protein_domains.iloc[number2, 3]
+
+                                Domain_start = Protein_domains.iloc[number2, 4]
+                                Domain_finish = Protein_domains.iloc[number2, 5]
+                                z = Start
+                                q = End
+                                x = Domain_start
+                                y = Domain_finish
+                                gene_peptide_domain = []
+                                if x <= z and z < y <= q or z <= x < q and y >= q or z <= x <= q and z <= y <= q or x <= z and y >= q and not x > q and y > q or not y < z and x < z:
+                                    # if this domain is fully within.
+                                    gene_peptide_domain = str(Gene + '(' + Domain_name + ')')
+
+                            if gene_peptide_domain == []:
+                                a_list_with_ids_of_peptide_cotaining_proteins = a_list_with_ids_of_peptide_cotaining_proteins + [
+                                    str(Gene + '(' + ' ' + ')')]
+                            else:
+                                a_list_with_ids_of_peptide_cotaining_proteins = a_list_with_ids_of_peptide_cotaining_proteins + [
+                                    str(gene_peptide_domain)]
 
 
-            #here we prepear for the export
-            peptide_proteins_domains = ';'.join(a_list_with_ids_of_peptide_cotaining_proteins)
-            new_column_same_sequences_bio_f = new_column_same_sequences_bio_f.append(
-                {'peptide_proteins_domains': peptide_proteins_domains}, ignore_index=True)
-        Peptides = pd.concat([Peptides.reset_index(), new_column_same_sequences_bio_f], axis=1)
-        Peptides.to_csv("All_The_ECM_Proteins/"+The_protein_gene_name_to_search_for+".csv", index=False)
-        #Export_function(Peptides)
+
+                    except:
+                        pass
+                    # print count
+                    count = count + 1
+
+
+                #here we prepear for the export
+                peptide_proteins_domains = ';'.join(a_list_with_ids_of_peptide_cotaining_proteins)
+                new_column_same_sequences_bio_f = new_column_same_sequences_bio_f.append(
+                    {'peptide_proteins_domains': peptide_proteins_domains}, ignore_index=True)
+            Peptides = pd.concat([Peptides.reset_index(), new_column_same_sequences_bio_f], axis=1)
+            Peptides.to_csv("All_The_ECM_Proteins/"+The_protein_gene_name_to_search_for+".csv", index=False)
+            #Export_function(Peptides)
 
 
